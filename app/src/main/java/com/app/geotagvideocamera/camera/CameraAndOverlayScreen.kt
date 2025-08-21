cfc22514412a9c85090bfb1141c3173cc12796fc
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-08-21 22:46:43 +0530

    Add a toast instead of starting the recording (android 14+ crashes due to extra perms)

--- app/src/main/java/com/app/geotagvideocamera/camera/CameraAndOverlayScreen.kt
+++ app/src/main/java/com/app/geotagvideocamera/camera/CameraAndOverlayScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.RadioButtonChecked
@@ -324,14 +325,14 @@ fun CameraAndOverlayScreen(
                             }
                         }
                         CameraMode.VIDEO -> {
-                            // Start screen recording via MediaProjection
-                            val intent = projectionManager.createScreenCaptureIntent()
-                            screenCaptureLauncher.launch(intent)
+                              Toast.makeText(context, "Use android's screen recording feature (from Settings or Quick Tiles", Toast.LENGTH_SHORT).show()
+//                            val intent = projectionManager.createScreenCaptureIntent()
+//                            screenCaptureLauncher.launch(intent)
                         }
                     }
                 },
                 modifier = Modifier
-                    .align(Alignment.BottomEnd)
+                    .align(Alignment.BottomCenter)
                     .padding(16.dp),
                 containerColor = Color(0xFF1E88E5),
                 contentColor = Color.White
